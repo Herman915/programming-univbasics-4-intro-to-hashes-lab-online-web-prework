d24a0070617d39d548a7f05ad17b339f601d801f
beatific-media-1104 <riostechnologies@gmail.com>
2020-01-17 23:17:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,17 +14,17 @@ def pioneer
 end
 
 def id_generator
-  id_generator = { id:  7 }
+  id_generator = { :id  7 }
   # return a hash with a key :id assigned to positive integer
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator = { age:  43 }
+  my_hash_creator = { :age  43 }
   # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
-  read_from_hash = { name: "Herman"}
+  read_from_hash = { :name "Herman"}
   # return the correct value using the hash and key parameters
 end
 
